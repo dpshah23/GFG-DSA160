aa729f94763ff1a399d60fee39f836a2c64887f5
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-12-06 10:32:59 +0530

    added day 18

---
+++ day 18 - Search Pattern (KMP-Algorithm).py
@@ -0,0 +1,33 @@
+class Solution:
+    def search(self, pat, txt):
+        def compute_lps(pattern):
+                lps = [0] * len(pattern)
+                length, i = 0, 1
+                while i < len(pattern):
+                    if pattern[i] == pattern[length]:
+                        length += 1
+                        lps[i] = length
+                        i += 1
+                    elif length != 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+                return lps
+    
+        lps = compute_lps(pat)
+        indices = []
+        i = j = 0
+        while i < len(txt):
+            if txt[i] == pat[j]:
+                i += 1
+                j += 1
+            if j == len(pat):
+                indices.append(i - j)
+                j = lps[j - 1]
+            elif i < len(txt) and txt[i] != pat[j]:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+        return indices
